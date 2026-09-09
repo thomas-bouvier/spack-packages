b09f21d5b6f8683eadd86a032ea71b1a26433a66
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-09-09 18:24:49 +0200

    koliop: add v0.2.1 (#6379)

--- repos/spack_repo/builtin/packages/koliop/package.py
+++ repos/spack_repo/builtin/packages/koliop/package.py
@@ -20,6 +20,7 @@ class Koliop(CMakePackage):
     license("MIT", checked_by="tpadioleau")
 
     version("master", branch="master", no_cache=True)
+    version("0.2.1", sha256="e47df0a03556be57a409b602a902b0b0df24d7edbf270270bdda1e7b49f55740")
     version("0.2.0", sha256="142733a74ff826d9afb5959f92550989bfbe08f6eba19ba893ccc069074235df")
     version("0.1.3", sha256="3c01ca0c4475bfceb2269713fc61c6bb328b1c2f7c8c262a6bdafc89d681244a")
     version("0.1.2", sha256="7581e8313b7ebc09b291ac486d7f6d03b55d7e0ce2e8ebf62863177bbb080fd1")
@@ -42,12 +43,13 @@ class Koliop(CMakePackage):
             self.define("koliop_ASSERT_ENABLED", True),
             self.define("koliop_ASSUME_INPUT_BUFFERS_ARE_DEVICE_COMPATIBLE", True),
             self.define("koliop_BUILD_FORTRAN_INTERFACE", False),
-            self.define("koliop_BUILD_TESTING", False),
+            self.define("koliop_BUILD_TESTING", self.run_tests),
             self.define("koliop_CONSTANT_MEMORY_USAGE", False),
             self.define("koliop_ENABLE_Kokkos", "SYSTEM"),
             self.define("koliop_ENABLE_KokkosKernels", "SYSTEM"),
             self.define("koliop_ENABLE_LTO", False),
             self.define("koliop_FENCE_ON_OPERATOR_EXIT", False),
+            self.define("koliop_RUN_DEMONSTRATION_AS_TEST", False),
             self.define("koliop_UTILITY_FLATTEN_EXPECT_POW2_EXTENTS", False),
         ]
 

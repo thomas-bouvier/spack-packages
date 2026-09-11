97aba28d428d491ffcccbb6b83acdd35bd926090
Greg Becker <becker33@llnl.gov>
2026-09-11 09:09:12 -0500

    kokkos: clean up usage of kokkos_cxx (#6417)
    * kokkos: more robust way of getting cxx compiler
    
    Signed-off-by: Gregory Becker <becker33@llnl.gov>
    
    * dealii: only use kokkos wrapper when trilinos is using it
    
    Signed-off-by: Gregory Becker <becker33@llnl.gov>
    
    * trilinos: deprecated kokkos_cxx attribute, use from kokkos package instead
    
    Signed-off-by: Gregory Becker <becker33@llnl.gov>
    
    ---------
    
    Signed-off-by: Gregory Becker <becker33@llnl.gov>

--- repos/spack_repo/builtin/packages/kokkos/package.py
+++ repos/spack_repo/builtin/packages/kokkos/package.py
@@ -445,8 +445,7 @@ class Kokkos(CMakePackage, CudaPackage, ROCmPackage):
     def kokkos_cxx(self) -> str:
         if self.spec.satisfies("+wrapper"):
             return self["kokkos-nvcc-wrapper"].kokkos_cxx
-        # Assumes build-time globals have been set already
-        return spack_cxx
+        return self["cxx"].cxx
 
     def cmake_args(self):
         spec = self.spec

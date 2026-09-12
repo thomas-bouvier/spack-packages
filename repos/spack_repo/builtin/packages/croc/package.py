3f034e3f7f3e0e64f94a4761a8c2ba0e8931bc84
Carlos Bederián <4043375+zzzoom@users.noreply.github.com>
2026-09-12 06:03:41 -0300

    croc: add v11.5.2 (#6429)
    Co-authored-by: Alec Scott <alec@llnl.gov>

--- repos/spack_repo/builtin/packages/croc/package.py
+++ repos/spack_repo/builtin/packages/croc/package.py
@@ -19,6 +19,10 @@ class Croc(GoPackage):
 
     license("MIT", checked_by="zzzoom")
 
+    version("11.5.2", sha256="2ceddb504be8b5912f4a3bfd76dc28bb18afda54191f2bebcf5b0fb24e63f774")
     version("10.2.5", sha256="993e0bb72e79c5168d78db5c14d84f69beeab819ab4d06f4d98fcddd23487207")
 
+    depends_on("go@1.27.0:", type="build", when="@11.2.3:")
+    depends_on("go@1.26.0:", type="build", when="@11.1.1:")
+    depends_on("go@1.25.0:", type="build", when="@10.4.0:")
     depends_on("go@1.24.0:", type="build", when="@10.2.5:")

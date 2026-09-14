eec495e3d28c45f7a1123c9a06052647ef948a4d
Lydéric Debusschère <lyderic.debusschere@asplus.fr>
2026-09-14 18:38:19 +0200

    py-conan: add v1.66.0 (#6391)

--- repos/spack_repo/builtin/packages/py_conan/package.py
+++ repos/spack_repo/builtin/packages/py_conan/package.py
@@ -15,19 +15,21 @@ class PyConan(PythonPackage):
 
     license("MIT")
 
+    version("1.66.0", sha256="04d2ad0f6ec6f055d95fa0a157b55fdea9328ae67f51b11e7d26e9d9d2f76ab9")
     version("1.52.0", sha256="184761f16d00fde17615e60125d2f14fca692ffba7666cc7d6d834fc3858cf82")
 
     depends_on("python@3.6:", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-requests@2.25:2", type=("build", "run"))
-    depends_on("py-urllib3@1.26.6:1.26", type=("build", "run"))
+    depends_on("py-urllib3@1.26.6:", type=("build", "run"))
     depends_on("py-colorama@0.3.3:0.4", type=("build", "run"))
     depends_on("py-pyyaml@3.11:6.0", type=("build", "run"))
     depends_on("py-patch-ng@1.17.4:1.17", type=("build", "run"))
     depends_on("py-fasteners@0.14.1:", type=("build", "run"))
     depends_on("py-six@1.10.0:1.16.0", type=("build", "run"))
     depends_on("py-node-semver@0.6.1", type=("build", "run"))
-    depends_on("py-distro@1.0.2:1.6.0", type=("build", "run"), when="platform=linux")
+    depends_on("py-distro@1.0.2:1.8.0", type=("build", "run"), when="@1.66: platform=linux")
+    depends_on("py-distro@1.0.2:1.6.0", type=("build", "run"), when="@1.52: platform=linux")
     depends_on("py-pygments@2.0:2", type=("build", "run"))
     depends_on("py-tqdm@4.28.1:4", type=("build", "run"))
     depends_on("py-jinja2@3.0:3", type=("build", "run"))

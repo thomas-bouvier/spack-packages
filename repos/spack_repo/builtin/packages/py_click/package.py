23a22a53984254039c06da8899509b478db2d550
Thomas Bouvier <contact@thomas-bouvier.io>
2026-09-21 09:00:01 +0200

    py-click: add missing dep

--- repos/spack_repo/builtin/packages/py_click/package.py
+++ repos/spack_repo/builtin/packages/py_click/package.py
@@ -16,6 +16,7 @@ class PyClick(PythonPackage):
 
     license("BSD-3-Clause")
 
+    version("8.5.0", sha256="ba0d2089de75ea0310e2dde03160e6ca10009947fb95a182f9b54021bb272e34")
     version("8.3.1", sha256="12ff4785d337a1bb490bb7e9c2b1ee5da3112e94a8622f26a6c77f5d2fc6842a")
     version("8.2.1", sha256="27c491cc05d968d271d5a1db13e3b5a184636d9d930f148c50b038f0d0646202")
     version("8.1.8", sha256="ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a")

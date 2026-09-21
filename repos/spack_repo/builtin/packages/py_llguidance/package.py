86e6abe4eaa7eb6d2ebbc9fee41f720bee7c6a81
Thomas Bouvier <contact@thomas-bouvier.io>
2026-09-21 10:52:15 +0200

    py-llguidance: add license

--- repos/spack_repo/builtin/packages/py_llguidance/package.py
+++ repos/spack_repo/builtin/packages/py_llguidance/package.py
@@ -13,6 +13,8 @@ class PyLlguidance(PythonPackage):
     homepage = "https://github.com/guidance-ai/llguidance"
     pypi = "llguidance/llguidance-1.6.1.tar.gz"
 
+    license("MIT")
+
     version("1.7.6", sha256="db7febbe412ed2015501904646750071d7e00e6df7f85c4b956ad4f206fd2df7")
     version("1.6.1", sha256="01611f85f834725fb359b568673cfaa5a352423779c097b03aa5ce68f0db3a51")
     version("1.3.0", sha256="861249afd51dc325646834462ea827e57a5c2b2042e108e6aae7059fdad9104d")

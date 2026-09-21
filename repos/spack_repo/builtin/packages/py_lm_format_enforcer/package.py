0930da2e3cd2d79167fbd62f8689e1f1a53868cc
Thomas Bouvier <contact@thomas-bouvier.io>
2026-09-21 12:48:15 +0200

    py-lm-format-enforcer: add missing bound

--- repos/spack_repo/builtin/packages/py_lm_format_enforcer/package.py
+++ repos/spack_repo/builtin/packages/py_lm_format_enforcer/package.py
@@ -18,7 +18,7 @@ class PyLmFormatEnforcer(PythonPackage):
     version("0.11.3", sha256="e68081c108719cce284a9bcc889709b26ffb085a1945b5eba3a12cfa96d528da")
 
     depends_on("python@3.8:", type=("build", "run"))
-    depends_on("py-poetry-core", type="build")
+    depends_on("py-poetry-core@1.1:", type="build")
     depends_on("py-pydantic@1.10.8:", type=("build", "run"))
     depends_on("py-interegular@0.3.2:", type=("build", "run"))
     depends_on("py-packaging", type="build")
